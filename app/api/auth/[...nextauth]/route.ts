03acf97349ede57d2fb387de2ec8a8c5dc435ebb
Peter <pzg1u18@soton.ac.uk>
2023-08-30 01:57:53 +0300

    Main page is set up

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,51 @@
+import NextAuth, { SessionOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+import User from '@/models/user'
+import { connectToDb } from '@/utils/database'
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({clientId: process.env.GOOGLE_ID as string, clientSecret: process.env.GOOGLE_CLIENT_SECRET as string})
+    ],
+    callbacks: {
+        
+    async session({session} : any) {
+
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+
+        session.user.id = sessionUser._id.toString()
+
+        return session;
+    },
+    async signIn({profile}: any) {
+        try {
+            // Every nextjs route is known as a serverless route which means it's a lambda function that opens up only when it gets called
+            // So every time it gets called it needs to spin up a connection to the server and access the database
+            await connectToDb();
+
+            //Check if a use already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            })
+            //If not, create a new use and save it to db
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+
+            return true;
+        } catch (error) {
+            console.log(error)
+            return false;
+        }
+    }
+    }
+})
+
+export { handler as GET, handler as POST }
