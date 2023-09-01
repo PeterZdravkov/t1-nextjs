37fbc95640fcf4d0751c83a430c1f7d00bd70dbd
Peter <peter.geotchliev@gmail.com>
2023-09-01 20:38:28 +0300

    Implemented search bar

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,16 +3,22 @@ import { useState, useEffect, ReactNode } from "react";
 
 import PromptCard from "@/components/PromptCard";
 
-const PromptCardList = ({ data, handleTagClick }: any) => {
+const PromptCardList = ({ data, handleTagClick, searchText }: any) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post: any) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
+      {data.map(
+        (post: any) =>
+          post.creator.username
+            .concat(post.creator.email, post.prompt, post.tag)
+            .toLowerCase()
+            .includes(searchText.toLowerCase()) && (
+            <PromptCard
+              key={post._id}
+              post={post}
+              handleTagClick={handleTagClick}
+            />
+          )
+      )}
     </div>
   );
 };
@@ -20,7 +26,11 @@ const PromptCardList = ({ data, handleTagClick }: any) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  const handleSearchChange: any = (e: Event) => {};
+
+  const handleSearchChange: any = (e: Event) => {
+    e.preventDefault();
+    setSearchText((e.target! as any).value);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -43,9 +53,24 @@ const Feed = () => {
           required
           className="search_input peer"
         />
+        <button
+          type="button"
+          className="outline_btn mx-6 bg-red-300"
+          onClick={() => {
+            setSearchText("");
+          }}
+        >
+          Clear
+        </button>
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList
+        data={posts}
+        handleTagClick={(tag: string) => {
+          setSearchText(tag);
+        }}
+        searchText={searchText}
+      />
     </section>
   );
 };
