57abf1ecf86ac5c750271853136c7d4ddae3b55a
Peter <peter.geotchliev@gmail.com>
2023-09-02 02:39:59 +0300

    update package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
-'use client'
-import { DetailedHTMLProps, FormHTMLAttributes, useMemo, useRef, useState } from "react";
-import Feed from '@/components/Feed'
-
+"use client";
+import {
+  DetailedHTMLProps,
+  FormHTMLAttributes,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import Feed from "@/components/Feed";
 
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover and Share 
-        <br className="max-md:hidden"/>
+        Discover and Share
+        <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> Prompts</span>
       </h1>
       <p className=" desc text-center">
-        Extremely simple website for creating, editing and deleting small prompts to practice app routing, api and next auth, mongoDB
+        Extremely simple website for creating, editing and deleting small
+        prompts to practice app routing, api and next auth, mongoDB
       </p>
       <Feed />
     </section>
-  )
+  );
 }
